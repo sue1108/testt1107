90cfba22eec36b829ef6c95c0d4dcf8dc541e625
suyx <sue1108@live.cn>
2011-11-11 11:31:03 +0800

    修改了第三题的代码，根据javadoc的规范，修改了注释

--- src/com/succez/test/testt1107/junit/TThreeTest.java
+++ src/com/succez/test/testt1107/junit/TThreeTest.java
@@ -8,14 +8,20 @@ import org.junit.Test;
 
 import com.succez.test.testt1107.three.ThreeClass;
 
-
+/**
+ * 
+ * <p>Copyright: Copyright (c) 2011<p>
+ * <p>第三题的测试类<p>
+ * @author sue
+ * @createdate 2011-11-10
+ */
 public class TThreeTest {
 
 	@Test
 	public void test() {
 //		fail("Not yet implemented");
 		File file = new File("d:\\test.txt");
-		ThreeClass.file2buf(file);
+		byte bt[] = ThreeClass.file2buf(file);  //将文件内容转换为字节数组
 //		Logger logger = 
 //		if (logger.isDebugEnabled()){    logger.debug("auto add private component[{}] to component's model.", depend);}
 		System.out.println("初始字符串:"+ThreeClass.getFileString(file));
